97d7c0264b044865e52c12c0317408e54d2bb41f
udovichenk0 <denzeldenisq@gmail.com>
2025-07-27 18:45:04 +0300

    simple lexer

---
+++ Lexer.cs
@@ -0,0 +1,101 @@
+
+enum TokenType
+{
+  Number,
+  Plus,
+  Minus,
+  Div,
+  Mul,
+  OpenParen,
+  CloseParen,
+}
+
+
+struct Token
+{
+  public required TokenType type;
+  public required string lit;
+}
+class Lexer
+{
+
+  string source;
+  List<Token> tokens = [];
+  int pos;
+  int start;
+
+  public Lexer(string Source)
+  {
+    source = Source;
+  }
+  void addToken(TokenType type)
+  {
+    string a = source[start..pos];
+    Token token = new Token()
+    {
+      type = type,
+      lit = source[start..pos],
+    };
+    tokens.Add(token);
+  }
+  public void Parse()
+  {
+    while (!IsEnd())
+    {
+      start = pos;
+      char c = Next();
+      switch (c)
+      {
+        case ' ':
+          continue;
+        case '+':
+          addToken(TokenType.Plus);
+          continue;
+        case '-':
+          addToken(TokenType.Minus);
+          continue;
+        case '*':
+          addToken(TokenType.Mul);
+          continue;
+        case '/':
+          addToken(TokenType.Div);
+          continue;
+        case '(':
+          addToken(TokenType.OpenParen);
+          continue;
+        case ')':
+          addToken(TokenType.CloseParen);
+          continue;
+      }
+      if (char.IsNumber(c))
+      {
+        string lit = c.ToString();
+        while (char.IsNumber(Peek()))
+        {
+          lit += Next();
+        }
+        addToken(TokenType.Number);
+        continue;
+      }
+    }
+  }
+
+  char Peek()
+  {
+    if (IsEnd()) return '\x00';
+    return source[pos];
+  }
+
+
+  char Next()
+  {
+    if (IsEnd()) return '\x00';
+    pos++;
+    return source[pos - 1];
+  }
+
+  bool IsEnd()
+  {
+    return pos == source.Length;
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace klang;
+
+public class Program
+{
+  static void Main()
+  {
+    string path = "./test";
+    try
+    {
+      using StreamReader reader = new(path);
+      string source = reader.ReadToEnd();
+
+      Lexer l = new Lexer(source);
+      l.Parse();
+
+    }
+    catch (IOException e)
+    {
+      Console.WriteLine($"Could not read file: {path}");
+      Console.WriteLine(e.Message);
+    }
+  }
+}
